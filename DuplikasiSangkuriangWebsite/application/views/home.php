f1508def44015b5ad6afb1d334a93c0b6feb1683
reyhanabbywahyu <reyhan.abbywahyu12@gmail.com>
2020-12-09 13:46:53 +0700

    init

---
+++ DuplikasiSangkuriangWebsite/application/views/home.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to SANGKURIANG INTERNATIONAL</title>
+</head>
+</html>
