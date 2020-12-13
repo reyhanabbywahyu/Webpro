85b22c4678754d7f789a4a8760e870061d53481f
reyhanabbywahyu <reyhan.abbywahyu12@gmail.com>
2020-12-13 09:41:15 +0700

    baru

--- DuplikasiSangkuriangWebsite/application/views/home.php
+++ DuplikasiSangkuriangWebsite/application/views/home.php
@@ -5,6 +5,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Sangkuriang Internasional - Innovative IT Solutions for You</title>
-	<h1>Hallo Semuanya</h1>
 </head>
 </html>
